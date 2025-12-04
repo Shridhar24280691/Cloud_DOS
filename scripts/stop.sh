d35653477b3533b92e41a3b959227542c1286c2f
EC2 Default User <ec2-user@ip-172-31-62-164.ec2.internal>
2025-12-04 12:57:38 +0000

    Force update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 echo "Stopping existing application processes..."
 
-# Stop Django dev server (if running)
 pkill -f "manage.py runserver" || true
-
-# Stop gunicorn (if running)
 pkill -f "gunicorn" || true
 pkill -f "wsgi:application" || true
 
