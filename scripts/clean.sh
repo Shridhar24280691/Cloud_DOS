8a30609fce6c993c6039901d3b84b9bac14650c5
EC2 Default User <ec2-user@ip-172-31-62-164.ec2.internal>
2025-12-04 12:00:31 +0000

    Restored working CI/CD scripts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rm -rf /var/www/car_app/*
-mkdir -p /var/www/car_app
+rm -rf /home/ec2-user/car_app/*
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/car_app
-
-# Kill old Django processes
-pkill -f "manage.py"
-
-# Start Django
 nohup python3 manage.py runserver 0.0.0.0:8080 > server.log 2>&1 &
