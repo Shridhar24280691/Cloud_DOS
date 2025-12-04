0c253ca6ccfc8e611fce11334355d79f02de95cb
EC2 Default User <ec2-user@ip-172-31-62-164.ec2.internal>
2025-12-04 13:06:13 +0000

    Fix permissions in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,7 @@ cd /home/ec2-user/car_app
 # Ensure log file is writable
 touch server.log
 chmod 666 server.log
-
+pkill -f "manage.py runserver" || true
 # Start Django in background
 nohup python3 manage.py runserver 0.0.0.0:8080 >> server.log 2>&1 &
+exit 0
