0e92daa87e9656e9b89c880ce3c28ae642c9e956
EC2 Default User <ec2-user@ip-172-31-60-48.ec2.internal>
2025-12-05 12:16:49 +0000

    added config

--- cardetailing/settings.py
+++ cardetailing/settings.py
@@ -83,7 +83,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
